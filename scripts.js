14dff2a1c6c03ce9031f454e1ce4e9f39d4ccd2c
TrishchanJimenez <trishchanjimenez06@gmail.com>
2023-11-27 10:50:38 +0800

    Split the carousel and offcanvas scripts into different js files

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Home Page carousel
 var index = 0; 
 var description = ["Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet."]
 
@@ -51,18 +52,3 @@ previousArrow.onclick = function() {
 } 
 
 dotArray[0].onclick = showSlides(1); 
-
-//Navigation Menu for small screen
-const openBtn = document.querySelector('.open-btn');
-const closeBtn = document.querySelector('.close-btn');
-const offcanvasMenu = document.querySelector('.offcanvas-menu')
-
-openBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    offcanvasMenu.classList.add('active');
-});
-
-closeBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    offcanvasMenu.classList.remove('active');
-});
