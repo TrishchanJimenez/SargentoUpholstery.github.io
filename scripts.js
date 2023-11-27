aacad29c90b69a5680bedee2efa5f70f4baeadc1
TrishchanJimenez <trishchanjimenez06@gmail.com>
2023-11-27 10:25:40 +0800

    Added offcanvas navigation bar for small screens

--- scripts.js
+++ scripts.js
@@ -51,3 +51,18 @@ previousArrow.onclick = function() {
 } 
 
 dotArray[0].onclick = showSlides(1); 
+
+//Navigation Menu for small screen
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const offcanvasMenu = document.querySelector('.offcanvas-menu')
+
+openBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    offcanvasMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    offcanvasMenu.classList.remove('active');
+});
