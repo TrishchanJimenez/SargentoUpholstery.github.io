a42c1daa58346270f07b6ddfec2a16d52fd5d507
TrishchanJimenez <trishchanjimenez06@gmail.com>
2024-01-02 18:56:05 +0800

    Slight changes in the productfiltering script

--- js/productfiltering.js
+++ js/productfiltering.js
@@ -1,21 +1,15 @@
 function filterGallery() {
-    var typeFilter = document.getElementById('type-selector').value;
-    var colorFilter = document.getElementById('color-selector').value;
+    const typeFilter = document.getElementById('type-selector').value;
+    const colorFilter = document.getElementById('color-selector').value;
+    const galleryItems = document.querySelectorAll('.gallery-item');
 
-    var galleryItems = document.getElementsByClassName('gallery-item');
-
-    for (var i = 0; i < galleryItems.length; i++) {
-        var item = galleryItems[i];
-
-        // Check if the item matches the selected filters
-        var typeMatch = typeFilter === 'all' || item.classList.contains(typeFilter);
-        var colorMatch = colorFilter === 'all' || item.classList.contains(colorFilter);
-
-        // Show or hide the item based on the filter matches
+    galleryItems.forEach((product) => {
+        let typeMatch = typeFilter === 'all' || product.classList.contains(typeFilter);
+        let colorMatch = colorFilter === 'all' || product.classList.contains(colorFilter);
         if (typeMatch && colorMatch) {
-            item.style.display = 'inline-block';
+            product.style.display = 'inline-block';
         } else {
-            item.style.display = 'none';
+            product.style.display = 'none';
         }
-    }
+    });
 }
