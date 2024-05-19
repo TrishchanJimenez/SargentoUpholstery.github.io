8c1c0b001e82629e1061a9fde31c2f2b221c38cd
Joaqamole96 <joaquinguevarra177@gmail.com>
2024-05-19 16:09:06 +0800

    alert_template.php and order form v5
    miss na kita

--- alert_template.php
+++ alert_template.php
@@ -1,7 +1,8 @@
 <?php
 function sendAlert($messageType, $message) {
     // alertTemplate.php
-    if (isset($message) && isset($messageType) && !empty($message) && !empty($messageType)) {
+    // success, error, warning, info
+    if ((isset($message) && isset($messageType) && !empty($message) && !empty($messageType))) {
         echo '<div class="alert alert--' . htmlspecialchars($messageType) . '">';
         echo '<span class="alert__message">' . htmlspecialchars($message) . '</span>';
         echo '<button class="alert__close-button">X</button>';
