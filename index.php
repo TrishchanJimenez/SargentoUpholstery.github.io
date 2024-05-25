787e3cab8e658c0e60d57274b6a01bbf6327ee2e
TrishchanJimenez <trishchanjimenez06@gmail.com>
2024-05-25 15:20:42 +0800

    Fix: Added missing global.js script

--- index.php
+++ index.php
@@ -88,6 +88,7 @@
         <a href="about_us.php" class="btn btn-white">Learn More</a>
     </div>
     <?php include_once("footer.php") ?>
- <script src="js/index/link-to-works.js"></script>
+    <script src="js/index/link-to-works.js"></script>
+    <script src="/js/globals.js"></script>
 </body>
 </html>
