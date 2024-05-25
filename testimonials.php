f26c5f27ffa0eb5ddb80c054d661d6cc03092740
Joaqamole96 <joaquinguevarra177@gmail.com>
2024-05-25 15:17:13 +0800

    Created template for featured banners

--- testimonials.php
+++ testimonials.php
@@ -1,6 +1,10 @@
 <?php
-    session_start(); // Start the session
-    require "database_connection.php";
+    // Start the session
+    if(session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    require("database_connection.php");
 
     // Function to get initials from username
     function getInitials($username) {
@@ -27,20 +31,32 @@
             R.reply,
             R.reply_date,
             GROUP_CONCAT(IFNULL(RI.path, '') SEPARATOR ',') AS review_images
-        FROM reviews R 
-        JOIN orders O ON R.order_id = O.order_id
-        JOIN users U ON U.user_id = O.user_id
-        LEFT JOIN review_images RI ON RI.review_id = R.review_id
-        GROUP BY R.review_id
-        ORDER BY R.review_id DESC;
+        FROM 
+            reviews R 
+        JOIN 
+            orders O ON R.order_id = O.order_id
+        JOIN 
+            users U ON U.user_id = O.user_id
+        LEFT JOIN 
+            review_images RI ON RI.review_id = R.review_id
+        GROUP BY 
+            R.review_id
+        ORDER BY 
+            R.review_id DESC;
     ";
     $result = $conn->query($sql);
     $reviews = $result->fetchAll(PDO::FETCH_ASSOC);
 
-    $sql = "SELECT ROUND(AVG(rating),1) AS avg_rating FROM reviews";
+    $sql = "
+        SELECT 
+            ROUND(AVG(rating),1) AS avg_rating 
+        FROM 
+            reviews
+    ";
     $stmt = $conn->query($sql);
     $avg_rating = ($stmt->fetch()['avg_rating'] / 5) * 100;
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,24 +67,12 @@
     <link rel="stylesheet" href="/css/testimonials.css">
     <link rel="stylesheet" href="/css/display_reviews.css">
 </head>
+
 <body>
-    <?php
-        include_once("header.php");
-        require 'database_connection.php';
-        $sql = "SELECT * FROM contents WHERE page = 'testimonials'";
-        $stmt = $conn->query($sql);
-        $contents = $stmt->fetchAll();
-        $contentsById = [];
-        foreach ($contents as $content) {
-            $contentsById[$content['content_id']] = $content;
-        }
+    <?php 
+        require_once('header.php');
+        require_once('sections/featured.php');
     ?>
-    <img src="/websiteimages/testimonialsfeaturedimg.png" alt="" class="testimonials-featured-img">
-    <div class="testimonials-title">
-        <h2><?= stripslashes(html_entity_decode($contentsById["TESTIMONIALHEADERTITLE"]["content_text"])) ?></h2>
-        <p><?= stripslashes(html_entity_decode($contentsById["TESTIMONIALHEADERSUBTEXT"]["content_text"])) ?></p>
-        <a href="order.php">Get a Free Quote</a>
-    </div>
     <img src="/websiteimages/Divider.png" class="testimonials-divider">
     <h2 class="star-rating-title">Our Average Star Rating</h2>
     <div class="flex-container">
@@ -86,119 +90,90 @@
             </div>
         </div>
     </div>
-    <!-- <div class="star-container">
-        <img src="/websiteimages/starimg.png" alt="">
-        <img src="/websiteimages/starimg.png" alt="">
-        <img src="/websiteimages/starimg.png" alt="">
-        <img src="/websiteimages/starimg.png" alt="">
-        <img src="/websiteimages/starimg.png" alt="">
-    </div> -->
-    <!-- <div class="review-container">
-        <div class="review">
-            <img src="/websiteimages/Start quote.png" alt="" class="quote quote-start">
-            <p>"Thank you po ang Ganda ng Sofa."<br><br><span class="client-name">-Lenard</span></p>
-            <img src="/websiteimages/End quote.png" alt="" class="quote quote-end">
-        </div>
-        <div class="review">
-            <img src="/websiteimages/Start quote.png" alt="" class="quote quote-start">
-            <p>"Maraming ty. Ang ganda."<br><br><span class="client-name">-Naej</span></p>
-            <img src="/websiteimages/End quote.png" alt="" class="quote quote-end">
-        </div>
-        <div class="review">
-            <img src="/websiteimages/Start quote.png" alt="" class="quote quote-start">
-            <p>"Thank you. Solid yung gawa , Malinis and quality."<br><br><span class="client-name">-Keith</span></p>
-            <img src="/websiteimages/End quote.png" alt="" class="quote quote-end">
-        </div>
-        <div class="review">
-            <img src="/websiteimages/Start quote.png" alt="" class="quote quote-start">
-            <p>"Maam thank you po, Happy costumer po ako, sa uulitin po."<br><br><span class="client-name">-Layla</span></p>
-            <img src="/websiteimages/End quote.png" alt="" class="quote quote-end">
-        </div>
-    </div>-->
     <div class='container'> 
         <div class="reviews-container">
             <?php
-            foreach($reviews AS $review) {
-                $date = date('M d, Y', strtotime($review['date']));
-                if(!is_null($review['reply_date'])) {
-                    $reply_date = date('M d, Y', strtotime($review['reply_date'])); 
-                }
-                
-                $initials = getInitials($review['name']);
-
-                $stars = "";
-                for ($i = 1; $i <= 5; $i++) {
-                    $stars .= "<span class='star'>";
-                    $stars .= $i <= (int)$review['rating'] ? '&#9733;' : '&#9734;';
-                    $stars .= "</span>";
-                }
-                
-                // var_dump($review['review_images']);
-                $review_images = "<div class='review-images'>"; 
-                if (!empty($review['review_images'])) {
-                    // echo "hello";
-                    $images = explode(',', $review['review_images']);
-                    foreach ($images as $image) {
-                        $review_images .= "<img src={$image} alt='Review Image'>";
+                foreach($reviews AS $review) {
+                    $date = date('M d, Y', strtotime($review['date']));
+                    if(!is_null($review['reply_date'])) {
+                        $reply_date = date('M d, Y', strtotime($review['reply_date'])); 
                     }
-                }
-                $review_images .= "</div>";
-                echo "
-                    <div class='review' id='review'>
-                        <div class='user-info'>
-                            <span class='avatar'>{$initials}</span>
-                            <span class='username'>{$review['name']}</span>
-                        </div>
-                        <div class='rating'>
-                            <div class='star-container'>
-                                {$stars}
-                            </div>
-                            <div class='date'>{$date}</div>
-                        </div>
-                        <div class='details'>
-                            Service: {$review['order_type']}<span> | </span>
-                            Furniture: {$review['furniture_type']}
-                        </div>
-                        <div class='review-text'>{$review['comment']}</div>
-                        {$review_images}
-                ";
-                if (empty($review['reply']) && $_SESSION['access_type'] === 'admin') {
+                    
+                    $initials = getInitials($review['name']);
+
+                    $stars = "";
+                    for ($i = 1; $i <= 5; $i++) {
+                        $stars .= "<span class='star'>";
+                        $stars .= $i <= (int)$review['rating'] ? '&#9733;' : '&#9734;';
+                        $stars .= "</span>";
+                    }
+                    
+                    // var_dump($review['review_images']);
+                    $review_images = "<div class='review-images'>"; 
+                    if (!empty($review['review_images'])) {
+                        // echo "hello";
+                        $images = explode(',', $review['review_images']);
+                        foreach ($images as $image) {
+                            $review_images .= "<img src={$image} alt='Review Image'>";
+                        }
+                    }
+                    $review_images .= "</div>";
                     echo "
-                        <input type='button' value='Reply' class='button-submit reply-button'>
-                        <form id='reply-form' class='reply-form' method='post'>
-                            <input type='hidden' name='review_id' value='{$review['review_id']}'>
-                            <textarea name='reply' id='' placeholder='Reply to this review...' class='text'></textarea>
-                            <div class='button-container'>
-                                <input type='submit' value='Save' class='button-submit submit-reply'>
-                                <input type='button' value='Cancel' class='button-submit cancel-reply'>
+                        <div class='review' id='review'>
+                            <div class='user-info'>
+                                <span class='avatar'>{$initials}</span>
+                                <span class='username'>{$review['name']}</span>
                             </div>
-                        </form>
+                            <div class='rating'>
+                                <div class='star-container'>
+                                    {$stars}
+                                </div>
+                                <div class='date'>{$date}</div>
+                            </div>
+                            <div class='details'>
+                                Service: {$review['order_type']}<span> | </span>
+                                Furniture: {$review['furniture_type']}
+                            </div>
+                            <div class='review-text'>{$review['comment']}</div>
+                            {$review_images}
                     ";
-                }
-                if (!empty($review['reply'])) { // Display admin reply if it exists
-                    echo "
-                        <div class='admin-reply'>
-                            <div class='admin-reply-container'>
-                                <div class='admin-reply-thing'> </div>
-                                <div class='info'>
-                                    <div class='topper'>Response: Sargento Upholstery - {$reply_date}</div>
-                                    <div class='reply'>{$review['reply']}</div>
+                    if (empty($review['reply']) && $_SESSION['access_type'] === 'admin') {
+                        echo "
+                            <input type='button' value='Reply' class='button-submit reply-button'>
+                            <form id='reply-form' class='reply-form' method='post'>
+                                <input type='hidden' name='review_id' value='{$review['review_id']}'>
+                                <textarea name='reply' id='' placeholder='Reply to this review...' class='text'></textarea>
+                                <div class='button-container'>
+                                    <input type='submit' value='Save' class='button-submit submit-reply'>
+                                    <input type='button' value='Cancel' class='button-submit cancel-reply'>
+                                </div>
+                            </form>
+                        ";
+                    }
+                    if (!empty($review['reply'])) { // Display admin reply if it exists
+                        echo "
+                            <div class='admin-reply'>
+                                <div class='admin-reply-container'>
+                                    <div class='admin-reply-thing'> </div>
+                                    <div class='info'>
+                                        <div class='topper'>Response: Sargento Upholstery - {$reply_date}</div>
+                                        <div class='reply'>{$review['reply']}</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ";
+                        ";
+                    }
+                    echo "</div>"; // Close review container
                 }
-                echo "</div>"; // Close review container
-            }
-            // Display reviews
-            if($result->rowCount() === 0) {
-                echo "<p>No reviews yet.</p>";
-            }
+                // Display reviews
+                if($result->rowCount() === 0) {
+                    echo "<p>No reviews yet.</p>";
+                }
             ?>
         </div>
     </div>
-    <?php include_once("footer.php") ?>
-    <script src="js/globals.js"></script>
+    <?php require_once('footer.php') ?>
+    <script src="/js/globals.js"></script>
     <script src="/js/display_reviews.js"></script>
 </body>
 </html>
