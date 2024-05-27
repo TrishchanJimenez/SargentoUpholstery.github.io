6051b8516d362db74605f67798a1cfe3cb02543a
TrishchanJimenez <trishchanjimenez06@gmail.com>
2024-05-27 18:04:56 +0800

    Feat: Added Quotations Table on Admin Side

---
+++ admin/quotation_details.php
@@ -0,0 +1,217 @@
+<?php
+    require '../database_connection.php';
+    session_start();
+    if(!isset($_SESSION['access_type']) || $_SESSION['access_type'] !== "admin") {
+        header("Location: ../index.php");
+        exit();
+    }
+
+    if(!isset($_GET['quote-id'])) {
+        header("Location: ./orders.php");
+        exit();
+    }
+    
+    $quote_id = $_GET['quote-id'];
+    $sql = "
+        SELECT 
+            * 
+        FROM 
+            quotes Q
+        LEFT JOIN
+            quote_customs QC USING(custom_id) 
+        JOIN
+            users U ON Q.customer_id = U.user_id
+        WHERE 
+            Q.quote_id = :quote_id
+    ";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':quote_id', $quote_id);
+    $stmt->execute();
+    $quote_details = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quote</title>
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/admin/quotations.css">
+</head>
+<body class="orders">
+    <?php require 'sidebar.php' ?>
+    <div class="order-detail">
+        <p class="main-title">Quote Detail</p>
+        <hr class="divider">
+        <div class="breadcrumb">
+            <a href="quotations.php">Quote </a>
+            <span> / <?= $quote_details['quote_id'] ?></span>
+        </div>
+        <div class="detail">
+            <div class="left">
+                <div class="order-information">
+                    <p class="info-title">
+                        QUOTE INFORMATION   
+                        <div class="info-order-detail">
+                    </p>
+                        <div class="info">
+                            <span class="info-name"> QUOTE ID </span>
+                            <span class="info-detail">
+                                <?= $quote_details['quote_id'] ?>
+                            </span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name"> SERVICE TYPE </span>
+                            <span class="info-detail">
+                                <?= $quote_details['service_type'] === "mto" ? "MTO" : "Repair" ?>
+                            </span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name"> FURNITURE TYPE </span>
+                            <span class="info-detail">
+                                <?= $quote_details['furniture_type'] ?>
+                            </span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name"> QUANTITY </span>
+                            <span class="info-detail">
+                                <?= $quote_details['quantity'] ?>
+                            </span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name"> QUOTE PLACEMENT DATE </span>
+                            <span class="info-detail">
+                                <?= date('M d, Y', strtotime($quote_details['created_at']))?>
+                            </span>
+                            </span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name"> QUOTE STATUS </span>
+                            <span class="info-detail status">
+                                <span <?= "data-prod-status='{$quote_details['quote_status']}'" ?> >
+                                    <?= ucfirst($quote_details['quote_status']) ?>
+                                </span>
+                            </span>
+                        </div>
+                        <?php if(!is_null($quote_details['quoted_price']) && $quote_details['quoted_price'] !== ''): ?>
+                            <div class="info">
+                                <span class="info-name">QUOTED PRICE</span>
+                                <span class="info-detail">
+                                    <?= '₱' . number_format($quote_details['quoted_price'], 2, '.', ',') ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                        <div class="info">
+                            <span class="info-name"> NOTE </span>
+                            <span class="info-detail">
+                                <?= $quote_details['description'] ?>
+                            </span>
+                        </div>
+                        <?php if(!is_null($quote_details['dimensions']) && $quote_details['dimensions'] !== ''): ?>
+                            <div class="info">
+                                <span class="info-name">DIMENSIONS</span>
+                                <span class="info-detail">
+                                    <?= $quote_details['dimensions'] ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                        <?php if(!is_null($quote_details['materials']) && $quote_details['materials'] !== ''): ?>
+                            <div class="info">
+                                <span class="info-name">MATERIALS</span>
+                                <span class="info-detail">
+                                    <?= $quote_details['materials'] ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                        <?php if(!is_null($quote_details['fabric']) && $quote_details['fabric'] !== ''): ?>
+                            <div class="info">
+                                <span class="info-name">FABRIC</span>
+                                <span class="info-detail">
+                                    <?= $quote_details['fabric'] ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                        <?php if(!is_null($quote_details['color']) && $quote_details['color'] !== ''): ?>
+                            <div class="info">
+                                <span class="info-name">COLOR</span>
+                                <span class="info-detail">
+                                    <?= $quote_details['color'] ?>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                        <?php if(!is_null($quote_details['ref_img_path'])): ?>
+                            <div class="info">
+                                <span class="info-name">PICTURE</span>
+                                <span class="info-detail">
+                                    <img src='/<?= $quote_details['ref_img_path'] ?>' alt='' class='repair-img'>
+                                </span>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="customer-information">
+                    <p class="info-title">
+                        CUSTOMER INFORMATION
+                    </p>
+                    <div class="info-order-detail customer-info">
+                        <div class="info">
+                            <span class="info-name">CUSTOMER NAME</span>
+                            <span class="info-detail"><?= $quote_details['name'] ?></span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name">EMAIL</span>
+                            <span class="info-detail"><?= $quote_details['email'] ?></span>
+                        </div>
+                        <div class="info">
+                            <span class="info-name">CONTACT NO.</span>
+                            <span class="info-detail"><?= $quote_details['contact_number'] ?></span>
+                        </div>
+                    </div>
+                </div>
+                <?php if($quote_details['quote_status'] === "pending") : ?>
+                    <div class='order-action'>
+                        <p class="info-title">
+                            ACTIONS
+                        </p>
+                        <div class="info-order-detail">
+                            <div class="action-buttons">
+                                <input type="button" value="quote" class="green-button accept-order action-button">
+                            </div>
+                            <form action="" method="post" id="order-accept-form">
+                                <input type="hidden" name="quote_id" <?= "value={$quote_id}" ?>>
+                                <div class="on-accept action-input">
+                                    <label for="price"> QUOTED PRICE</label>
+                                    <input type="number" name="price" class="price-input" placeholder="₱12131" required>
+                                </div>
+                                <div class="on-click">
+                                    <input type="submit" value="save" class="green-button action-button">
+                                    <input type="button" value="cancel" class="red-button action-button">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <script>let quoteId = <?= $quote_details['quote_id'] ?></script>
+    <script src="/js/quotation.js"></script>
+</body>
+</html>
+<?php
+    include_once("../notif.php");
+    if($_SERVER['REQUEST_METHOD'] === "POST") {
+        $quote_id = $_POST['quote_id'];
+        $price = $_POST['price'];
+
+        $stmt = $conn->prepare("UPDATE quotes SET quote_status = 'approved', quoted_price = :price WHERE quote_id = :quote_id");
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':quote_id', $quote_id);
+        $stmt->execute();
+        createNotif($quote_details['customer_id'], "Your request for quotation has been evaluated", "/my/user_order_details.php?quote-id=" . $quote_details['quote_id']);
+        // header("Location: ".$_SERVER['PHP_SELF']);
+        // exit();
+    }
+?>
---
+++ admin/quotations.php
@@ -0,0 +1,87 @@
+<?php
+    require '../database_connection.php';
+    session_start();
+    if(!isset($_SESSION['access_type']) || $_SESSION['access_type'] === "customer"){
+        header("Location: ../index.php");
+        exit();
+    }
+
+    $quotationSql = "
+        SELECT
+            quote_id,
+            furniture_type AS item,
+            name AS customer_name,
+            service_type AS type,
+            quantity,
+            quote_status AS status,
+            created_at AS placement_date
+        FROM
+            quotes Q
+        JOIN users O ON Q.customer_id = O.user_id  
+        WHERE
+            quote_status IN ('pending', 'approved')
+    ";
+
+    $stmt = $conn->prepare($quotationSql);
+    $stmt->execute();
+    $quotations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quotations</title>
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/admin/quotations.css">
+</head>
+<body>
+    <div class="orders">
+        <?php require 'sidebar.php' ?>
+        <div class="order-list">
+            <p class="main-title">Request For Quotes</p>
+            <hr class="divider">
+            <table class="order-table">
+                <thead>
+                    <tr>
+                        <th>Quote Id</th>
+                        <th>Customer Name</th>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Type</th>
+                        <th>Placement Date</th>
+                        <th>Status</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($quotations AS $quote) {
+                            $date = date('M d, Y', strtotime($quote['placement_date']));
+                            $type = ($quote['type'] === "mto") ? "MTO" : "Repair";
+                            $status = $quote['status'];
+                            $status_text = ($status === "pending") ? "Pending" : "Approved";
+                            echo "
+                            <tr data-id='{$quote['quote_id']}'>
+                                <td>{$quote['quote_id']}</td>
+                                <td>{$quote['customer_name']}</td>
+                                <td>{$quote['item']}</td>
+                                <td>{$quote['quantity']}</td>
+                                <td>{$type}</td>
+                                <td>{$date}</td>
+                                <td class='prod-status status'>
+                                    <span data-prod-status='{$status}'>{$status_text}</span>
+                                </td>
+                                <td class='chevron-right'>
+                                    <a href='quotation_details.php?quote-id={$quote['quote_id']}'>
+                                        <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#6B7280'><path d='M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z'/></svg>
+                                    </a>
+                                </td>
+                            </tr>
+                            ";
+                        }
+                    ?>
+                </tbody>
+            </table>
+</body>
+</html>
